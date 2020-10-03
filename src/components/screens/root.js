587967d6e15f48aa6f8f47d94269b334a4e10e23
Erwan <erwan.sinou@epitech.eu>
2020-10-03 11:15:35 +0200

    [ADD] project base

---
+++ src/components/screens/root.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../../css/epinasa.css'
+
+export default class Root extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+    componentDidMount() {
+
+    }
+    render() {
+        return (
+            <div className="globalContainer">
+
+            </div>
+        )
+    }
+}
